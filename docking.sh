af00727a6d9ed32dc4a0ba791de197c94355dc81
qxie9876 <987671584@qq.com>
2019-12-23 16:26:19 +0800

    bug fix

--- docking.sh
+++
@@ -1,5 +0,0 @@
-export PATH=/home/qxie/softwares/mgltools_1.5.6/bin/:$PATH
-export PATH=/home/qxie/softwares/autodock_4_2_6/:$PATH
-export PATH=/home/qxie/softwares/autodock_vina_1_1_2/bin/:$PATH
-
-
